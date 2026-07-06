a316be29f665da6eb93880bf1137935ec5c251c9
Jatin Mehrotra <jatimeh@amazon.com>
2026-07-06 17:08:07 +0530

    fix: add force_destroy to S3 bucket for clean terraform destroy

--- Blog/dynamic-pr-environment/infra/main.tf
+++ Blog/dynamic-pr-environment/infra/main.tf
@@ -20,6 +20,7 @@ data "aws_caller_identity" "current" {}
 
 resource "aws_s3_bucket" "artifacts" {
   bucket = "${var.project_name}-artifacts-${data.aws_caller_identity.current.account_id}"
+  force_destroy = true
 
   tags = {
     Project = var.project_name
